444b2de4477b7559f0a65d15a83422241bf588b7
Bart Wyatt <bart@coretechfoundation.org>
2017-07-19 11:41:09 -0400

    Moved trace sampling to a thread, records as binary to a tempfile for speed then transcribes to json at the end

--- src/include/util/scope_profile.hpp
+++ src/include/util/scope_profile.hpp
@@ -1,64 +1,78 @@
 #pragma once
-#include <fstream>
+#include <algorithm>
 #include <chrono>
-#include <boost/format.hpp>
+#include <cstdio>
 #include <thread>
 
 namespace sched_bench { namespace util {
 
     namespace scope_profile {
-        extern std::ofstream profile_trace;
-        extern char const *trace_events_separator;
-        extern std::chrono::steady_clock::time_point epoch;
-        bool init(char const *filename);
+        enum class Sample_record_type {
+            INVALID,
+            BEGIN,
+            END,
+        };
+
+        struct Sample_record_header {
+            std::chrono::steady_clock::time_point tp;
+            uint tid;
+            Sample_record_type type;
+        };
+
+        struct Sample_record : public Sample_record_header {
+            static const uint DESIRED_RECORD_SIZE = 256;
+            static const uint MAX_NAME_LENGTH = DESIRED_RECORD_SIZE - sizeof(Sample_record_header);
+            char name[MAX_NAME_LENGTH];
+        };
+
+        static_assert(sizeof(Sample_record) == Sample_record::DESIRED_RECORD_SIZE, "unexpected size of struct Sample_record");
+
+        bool init(char const *filename, uint num_sample_record = 4096);
         void add_metadata(char const *key, char const *value);
         uint get_thread_id();
+        Sample_record &get_record();
         void shutdown();        
 
         template<typename ARG>
-        void emit_sample_name(ARG arg) {
-            profile_trace << arg;
+        struct __format_string {
+        };
+
+        template<typename ARG>
+        void record_sample_name(char *buf, uint &offset, uint capacity, ARG arg) {
+            if (capacity - offset < 1) {
+                return;
+            }
+            offset = std::min(capacity, offset + std::snprintf(buf+offset, capacity-offset, __format_string<ARG>::value, arg));
         }
 
         template<typename ARG, typename ...ARGS>
-        void emit_sample_name(ARG arg, ARGS... args) {
-            profile_trace << arg;
-            emit_sample_name(args...);
+        void record_sample_name(char *buf, uint &offset, uint capacity, ARG arg, ARGS... args) {
+            record_sample_name(buf, offset, capacity, arg);
+            record_sample_name(buf, offset, capacity, args...);
         }
 
         template<typename ...ARGS>
         void emit_sample_begin(ARGS... args) {
             auto now = std::chrono::steady_clock::now();
-            uint64_t now_us = std::chrono::duration_cast<std::chrono::microseconds>(now - epoch).count();
             auto tid = get_thread_id();
+            auto &record = get_record();
             
-            profile_trace 
-                << trace_events_separator
-                << boost::format { "{\"tid\":%s,\"pid\":%s,\"ts\":%d,\"ph\":\"B\",\"cat\":\"P\",\"name\":\"" }
-                % tid
-                % tid
-                % now_us
-                ;
-
-            emit_sample_name(args...);
-            profile_trace <<  "\" }";
-            trace_events_separator = ",\n";
+            uint offset = 0;
+            record_sample_name(record.name, offset, Sample_record::MAX_NAME_LENGTH, args...);
+            
+            record.tp = now;
+            record.tid = tid;
+            record.type = Sample_record_type::BEGIN;
         }
 
         inline
         void emit_sample_end() {
-            auto now = std::chrono::steady_clock::now();
-            uint64_t now_us = std::chrono::duration_cast<std::chrono::microseconds>(now - epoch).count();
             auto tid = get_thread_id();
-            profile_trace 
-                << trace_events_separator
-                << boost::format { "{\"tid\":%s,\"pid\":%d,\"ts\":%d,\"ph\":\"E\"}" }
-                % tid
-                % tid
-                % now_us
-                ;
-
-            trace_events_separator = ",\n";
+            auto &record = get_record();
+            auto now = std::chrono::steady_clock::now();
+            record.tp = now;
+            record.tid = tid;
+            record.type = Sample_record_type::END;
         }
 
         class Sample {
@@ -85,6 +99,36 @@ namespace sched_bench { namespace util {
         #define SCOPE_PROFILE(...) auto UNIQUE_SCOPE = ::sched_bench::util::scope_profile::Sample::create(__VA_ARGS__)
         #define SCOPE_PROFILE_FUNCTION() auto UNIQUE_SCOPE = ::sched_bench::util::scope_profile::Sample::create(__FUNCTION__)
         
+
+        template<>
+        struct __format_string<char const *> {
+            static constexpr char const * value = "%s";
+        };
+
+        template<>
+        struct __format_string<char *> {
+            static constexpr char const * value = "%s";
+        };
+
+        template<>
+        struct __format_string<uint> {
+            static constexpr char const * value = "%d";
+        };
+
+        template<>
+        struct __format_string<int> {
+            static constexpr char const * value = "%d";
+        };
+
+        template<>
+        struct __format_string<float> {
+            static constexpr char const * value = "%0.04f";
+        };
+
+        template<>
+        struct __format_string<double> {
+            static constexpr char const * value = "%0.04f";
+        };
     }
 
 }}
